1babc1e459f9f409695ffe8a5b1d990b16ffe2c7
radomir.sebek <rasho.ieee@gmail.com>
2018-10-03 12:34:50 +0200

    adding check for replication_id

---
+++ analyseReplicationResult.js
@@ -0,0 +1,12 @@
+/* eslint-disable max-len */
+const replicationResult = require('./replicationResult.json');
+const Utilities = require('./modules/Utilities');
+require('dotenv').config();
+const _ = require('lodash-uuid');
+
+if (_.isUuid4(replicationResult.replication_id)) {
+    console.log('replication_id is OK');
+} else {
+    console.log('replication_id is not OK');
+}
+
