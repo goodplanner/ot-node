3089ebd2a62d401a0a1f7620860e10c65a9ebcb0
radomir.sebek <rasho.ieee@gmail.com>
2018-10-03 11:23:52 +0200

    rename postResult to importResult

--- analyseImportResult.js
+++ analyseImportResult.js
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
-const postResult = require('./postResult.json');
-const Utilities = require('./modules/Utilities.js');
+const postResult = require('./importResult.json');
+const Utilities = require('./modules/Utilities');
 require('dotenv').config();
 
 if (!Utilities.isHexStrict(postResult.import_hash) || Utilities.isZeroHash(postResult.import_hash)) {
@@ -15,7 +15,7 @@ if (!Utilities.isHexStrict(postResult.import_id) || Utilities.isZeroHash(postRes
     process.exit(-1);
 } else {
     console.log('import_id is OK');
-    process.env.IMPORT_ID = postResult.import_id;
+    // TODO process.env.IMPORT_ID = postResult.import_id;
     console.log(process.env.IMPORT_ID);
 }
 
@@ -26,10 +26,11 @@ if (postResult.message !== 'Import success') {
     console.log('message is OK');
 }
 
-if (postResult.wallet !== process.env.NODE_WALLET) {
-    console.log('wallet is not OK');
-    process.exit(-1);
-} else {
-    console.log('wallet is OK');
-}
+// TODO
+// if (postResult.wallet !== process.env.NODE_WALLET) {
+//     console.log('wallet is not OK');
+//     process.exit(-1);
+// } else {
+//     console.log('wallet is OK');
+// }
 
